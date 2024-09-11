ffbb7f40364ee86c4ae935c84c07fdeff18786bc
demzyAkpan <demzyukonta@gmail.com>
2024-09-11 15:52:48 +0100

    new

--- website/models.py
+++ website/models.py
@@ -13,4 +13,4 @@ class Record(models.Model):
 
 
     def __str__(self):
-        return(f'{self.first_name} {self.last_name}')
+        return f'{self.first_name} {self.last_name}'
--- website/views.py
+++ website/views.py
@@ -1,28 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm 
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
  
 def home(request):
-    #grabs every item in the databse table and assigns it to this variable
     records = Record.objects.all()
 
-    #check to see if logging in
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        #Authenticate
-        user = authenticate(request, username=username, password =password)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, "You are now logged in")
             return redirect('home')
         else:
-            messages.success(request, "There was an error logging in, please enter the correct etails and try again")
+            messages.error(request, "Invalid login details. Please try again.")
             return redirect('home')
-    else:
-        return render(request, 'home.html', {'records':records}) #empty context dictionary{}
+    return render(request, 'home.html', {'records': records})
+ #empty context dictionary{}
 
 # def login_user(request):
 #     pass
@@ -38,20 +35,46 @@ def register_user(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            #Authenticate and Login
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(request, 'You have been successfully registered')
-            return redirect('home')   
+            return redirect('home')
     else:
         form = SignUpForm()
-        return render(request, 'register.html', {'form': form   })
-    return render(request, 'register.html', {'form': form  })
+
+    return render(request, 'register.html', {'form': form})
+
 
 def customer_records(request, pk):
     if request.user.is_authenticated:
         #look up records
         customer_records = Record.objects.get(id=pk)
-        
+        return render(request, 'record.html', {'customer_records': customer_records})
+    else:
+        messages.success(request, 'Must be logged in to view this page ')
+        return redirect('home')  
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk) 
+        delete_it.delete()
+        messages.success(request, 'Record Deleted Successfully ')
+        return redirect('home') 
+    else:
+        messages.success(request, 'You must be logged in before you can perform this action')
+        return redirect('home')
+
+def add_record(request):
+    if request.user.is_authenticated:
+        form = AddRecordForm(request.POST or None)
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Record added successfully')
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.error(request, 'You must be logged in to add a record')
+        return redirect('home')
