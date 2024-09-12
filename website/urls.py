5cfb8c10f719d4b97d24344c6aee61faf4567e6c
demzyAkpan <demzyukonta@gmail.com>
2024-09-12 13:22:17 +0100

    new

--- website/urls.py
+++ website/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('register/',views.register_user, name = 'register'),
     path('record/<int:pk>',views.customer_records, name = 'record'),
     path('delete_record/<int:pk>',views.delete_record, name = 'delete'),
-     path('add_record/',views.add_record, name = 'add_record'),
+    path('add_record/',views.add_record, name = 'add_record'),
+    path('update_record/<int:pk>',views.update_record, name = 'update_record'),
 ]
 
 
--- website/views.py
+++ website/views.py
@@ -58,7 +58,7 @@ def customer_records(request, pk):
 
 def delete_record(request, pk):
     if request.user.is_authenticated:
-        delete_it = Record.objects.get(id=pk) 
+        delete_it = Record.objects.get(id=pk)
         delete_it.delete()
         messages.success(request, 'Record Deleted Successfully ')
         return redirect('home') 
@@ -75,6 +75,19 @@ def add_record(request):
                 messages.success(request, 'Record added successfully')
                 return redirect('home')
         return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.error(request, 'You must be logged in to add a record')
+        return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.error(request, 'Record updated sucessfully')
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
     else:
         messages.error(request, 'You must be logged in to add a record')
         return redirect('home')
